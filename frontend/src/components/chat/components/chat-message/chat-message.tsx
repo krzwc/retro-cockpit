bdedb197d35071c14c57d26478af43acf784cc4c
Kacper Krzywiec <krzywiec@gmail.com>
2020-07-27 18:28:52 +0200

    Add bot avatar

--- frontend/src/components/chat/components/chat-message/chat-message.tsx
+++ frontend/src/components/chat/components/chat-message/chat-message.tsx
@@ -8,10 +8,17 @@ import styles from './style.scss';
 
 const leftOrRight = (name: string) => (name === DEFAULT_USER ? styles.right : styles.left);
 const leftOrRightBaloon = (name: string) => (name === DEFAULT_USER ? 'from-right' : 'from-left');
+const isBot = (name: string) => name === 'BOT';
 
 const ChatMessage: FunctionComponent<Message> = ({ name, text }) => {
     return (
         <section className={classNames('message', leftOrRight(name), styles.message)}>
+            {isBot(name) && (
+                <img
+                    className={classNames('nes-avatar', 'is-rounded', 'is-large', styles.avatar)}
+                    src="assets/images/robot.png"
+                />
+            )}
             <div className={classNames('nes-balloon', leftOrRightBaloon(name), 'is-dark')}>
                 <p>{text}</p>
             </div>
