3a572bbb33885ca7b5d334821d84fe0dc78caccd
Kacper Krzywiec <krzywiec@gmail.com>
2020-09-03 19:10:32 +0200

    Add dockerize to pipeline

--- timescaledb/init.sql
+++ timescaledb/init.sql
@@ -1,26 +1,20 @@
 CREATE TABLE IF NOT EXISTS alarms (
-    id SERIAL,
     timestamp TIMESTAMPTZ NOT NULL,
     severity CHAR(10),
     resolved BOOLEAN,
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS pb_metrics (
-    id SERIAL,
+    timestamp TIMESTAMPTZ NOT NULL,
     pb CHAR(3),
     value SMALLINT,
-    timestamp TIMESTAMPTZ NOT NULL,
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS bc_metrics (
-    id SERIAL,
+    timestamp TIMESTAMPTZ NOT NULL,
     core CHAR(5),
     freq0 SMALLINT,
     freq1 SMALLINT,
-    timestamp TIMESTAMPTZ NOT NULL,
-    PRIMARY KEY (id)
 );
 
 SELECT create_hypertable('alarms', 'timestamp');
